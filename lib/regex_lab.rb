d79013da3e0ba8c851c96c80379772a04c1874c5
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-31 20:23:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,10 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  rarray = []
+  if text.match(/^[un]|$[ing]/)
+    rarray << text
+  end
 end
 
 def words_five_letters_long(text)
