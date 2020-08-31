a6a36631e220db65e990f0fd22103f53de47b876
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-31 20:28:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/^\w{5}\s/)
+  rarray = text.split(" ")
+  rarray.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
